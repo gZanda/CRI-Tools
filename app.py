ce15e756436b6dc84641c89120017c24a5e706d1
gZanda <gabrielfzanda@gmail.com>
2025-08-11 11:07:19 -0300

    feat: Main application panel

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+import tkinter as tk
+import subprocess
+from tkinter import filedialog, messagebox, ttk
+
+def calc():
+    subprocess.run( ["python", "C:/Users/gabriel.goncalves/Desktop/CRI-Tools/calcular_vencimento.py"], check=True)
+
+
+# Empty dir
+def empty_dir():
+    subprocess.run(["python", "C:/Users/gabriel.goncalves/Desktop/CRI-Tools/empty_dir.py"], check=True)
+
+# DOI
+def doi():
+    subprocess.run(["python", "C:/Users/gabriel.goncalves/Desktop/CRI-Tools/DOI Summary/doi_script.py"], check=True)
+
+
+# Interface
+janela = tk.Tk()
+janela.title("Selecione sua Ferramenta")
+janela.geometry("600x450")
+
+btn_escolher1 = tk.Button(janela, text="Calcular Vencimento de Contrato", command=calc, font=("Arial", 12))
+btn_escolher1.pack(pady=10)
+
+btn_escolher2 = tk.Button(janela, text="Listar Diretórios Vazios", command=empty_dir, font=("Arial", 12))
+btn_escolher2.pack(pady=10)
+
+btn_escolher3 = tk.Button(janela, text="Relatório DOI", command=doi, font=("Arial", 12))
+btn_escolher3.pack(pady=10)
+
+janela.mainloop()
